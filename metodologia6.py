74edee287547d71eb6075c29e2ff2840dee9c7c4
Mejiao-o <letmedienowplis@gmail.com>
2026-03-25 12:02:15 -0500

    second commit

--- metodologia6.py
+++ metodologia6.py
@@ -5,4 +5,4 @@ def metodologia():
     print(f"{Fore.CYAN}6. Metodologia")
     print("")
     print("Utilizamos nuestros conocimientos en el funcionamiento de la visión artificial para desarrollar un programa capaz de identificar los distintos residuos que se le puedan mostrar y dar como resultado en que caneca irían destinados cada uno de estos residuos.  ")
-    print("")
+    print("AE<ZSWDXRFCTGV")
